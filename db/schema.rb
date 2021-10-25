eba020aba32a4446eb5b4ccb8a4889116e7ca317
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-26 01:49:12 +0300

    add default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_224222) do
+ActiveRecord::Schema.define(version: 2021_10_25_224645) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct", null: false
+    t.boolean "correct", default: false, null: false
     t.integer "questions_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_224222) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 1, null: false
     t.integer "categories_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
