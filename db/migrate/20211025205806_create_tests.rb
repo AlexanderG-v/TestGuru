0b79404a00d0ec8d3844415dd5142e9285824d37
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-26 13:18:55 +0300

    elimination of comments

--- db/migrate/20211025205806_create_tests.rb
+++ db/migrate/20211025205806_create_tests.rb
@@ -1,9 +1,10 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
-      t.references :categories, null: false, foreign_key: true
+      t.string :title, null: false
+      t.integer :level, default: 1, null: false
+      t.string :author, null: false
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20211025211045_create_questions.rb
+++ db/migrate/20211025211045_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.string :body
-      t.references :tests, null: false, foreign_key: true
+      t.string :body, null: false
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
