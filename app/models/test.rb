be5003d8736bcb523656f8717487aa94e71a9900
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-29 00:07:03 +0300

    created method 'self.list_tests_by_category'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.array_of_test_names(title_category)
+    joins('INNER JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: title_category }).order(id: :desc).pluck(:title)
+  end
 end
