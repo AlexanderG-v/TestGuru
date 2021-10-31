e0f8df275add4064093b181546bc6e5d0f33670f
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-31 20:05:08 +0300

    add association test-author and

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,11 @@
 class Test < ApplicationRecord
 
-  has_many :questions
-  has_many :tests_users
-  has_many :users, through: :tests_users
   belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :users, through: :tests_users, dependent: :destroy
 
   def self.list_tests_by_category(title_category)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def list_tests_by_level(level)
     Test.joins('JOIN tests_users ON tests_users.test_id = tests.id')
