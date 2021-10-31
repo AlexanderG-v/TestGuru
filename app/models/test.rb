b5208baa755896abb0446757089da50e2d9a9e02
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-31 17:01:45 +0300

    add association category-tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,7 @@
 class Test < ApplicationRecord
+
+  belongs_to :category
+
   def self.list_tests_by_category(title_category)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: title_category })
