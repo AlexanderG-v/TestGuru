e0d2a1c576df9df5a7e283328949770aab7748af
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-02 19:49:41 +0300

    created scope method list_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  def list_tests_by_level(level)
-    tests.where(level: level)
-  end
+  scope :list_tests_by_level, -> (level) { tests.where(level: level) }
+
+  #def list_tests_by_level(level)
+  #  tests.where(level: level)
+  #end
 end
