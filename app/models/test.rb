f25e4bfb83ee3ff640a739521b781883f7200c20
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-02 19:10:10 +0300

    created scope method for selecting level of tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,10 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
+  scope :easy_tests, -> { where(level: 0..1) }
+  scope :mid_tests, -> { where(level: 2..4) }
+  scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
+
   def self.list_tests_by_category(title_category)
     joins(:category)
       .where(categories: { title: title_category })
