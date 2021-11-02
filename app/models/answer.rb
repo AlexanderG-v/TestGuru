2f20044166d8d459786d2774792be5877f93d2f2
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-03 02:18:44 +0300

    add validates for Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,13 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
-  scope :correct_answer, -> { where(correct: 1) }
+  validate :validate_answer_count, on: :create
+
+  scope :correct_answer, -> { where(correct: true) }
+
+  private
+
+  def validate_answer_count
+    errors.add(:answers_count, 'Должно быть от 1-го до 4-х ответов!') if question.answers.count >= 4
+  end
 end
