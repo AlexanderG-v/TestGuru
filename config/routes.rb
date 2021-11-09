22aafc1a7a8b9528dde320ca3eae9acfaf5aad21
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-09 16:48:23 +0300

    fixed AnswersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :answers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
