faa12653444936ebbbd875cfd5077851c5f9ed66
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-15 19:37:40 +0300

    implemented authentication using a 'device'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  helper_method :current_user,
-                :logged_in?
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      cookies[:original_path] = request.path
-      redirect_to login_path, alert: 'Verify Email and Password please'
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
-
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :validatable,
+         :confirmable
 
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
-
-  has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true
