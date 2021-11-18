4be180d93f69ca6341dd68db3c626e836388b050
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-19 02:01:04 +0300

    localization of texts for the admin is implemented

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -15,7 +15,7 @@ class Admin::QuestionsController < Admin::BaseController
     @question = @test.questions.new(question_params)
    
     if @question.save
-      redirect_to admin_question_path(@question)
+      redirect_to admin_question_path(@question), notice: t('.success')
     else
       render :new
     end
@@ -25,7 +25,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def update
     if @question.update(question_params)
-      redirect_to admin_question_path(@question)
+      redirect_to admin_question_path(@question), notice: t('.success')
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class Admin::QuestionsController < Admin::BaseController
   def destroy
     @question.destroy
     
-    redirect_to admin_test_path(@question.test)
+    redirect_to admin_test_path(@question.test), notice: t('.success')
   end
 
   private
