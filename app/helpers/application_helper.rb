56c58cd2606de76bcedb3aef19dd93cb12fb1d7f
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-11-22 19:10:51 +0300

    fixed flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def current_year
     Date.current.year
   end
 
   def github_url(author, repo = nil)
-    title = repo.nil? ? "#{t('author', author: author)}" : "#{t('project', project: repo)}"
+    title = repo.nil? ? t('author', author: author).to_s : t('project', project: repo).to_s
     link_to title, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
+
+  def flash_class(key)
+    case key
+    when 'notice' then 'alert alert-info'
+    when 'alert' then 'alert alert-danger'
+    end
   end
 end
