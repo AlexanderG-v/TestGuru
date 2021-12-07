7c16417823f5ee83683f7d2dfce46fb9dc8144ba
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-07 19:25:44 +0300

    created a Badge model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_183606) do
+ActiveRecord::Schema.define(version: 2021_12_07_153926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2021_12_03_183606) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["body"], name: "index_answers_on_body"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image", null: false
+    t.string "rule_name", null: false
+    t.string "rule_value", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
