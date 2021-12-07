74ecdde52efb9c0e1d3943ffe74f6fe2632719b3
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-07 20:01:10 +0300

    created the join table 'badges_users' and implemented the associations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Badge < ApplicationRecord
+  has_many :badges_users, dependent: :destroy
+  has_many :users, through: :badges_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_153926) do
+ActiveRecord::Schema.define(version: 2021_12_07_162856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_12_07_153926) do
     t.string "rule_value", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "badges_users", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "badge_id", null: false
+    t.index ["badge_id"], name: "index_badges_users_on_badge_id"
+    t.index ["user_id"], name: "index_badges_users_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -120,6 +127,8 @@ ActiveRecord::Schema.define(version: 2021_12_07_153926) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "badges_users", "badges"
+  add_foreign_key "badges_users", "users"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "gists", "questions"
   add_foreign_key "gists", "users"
