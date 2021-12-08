31bb9f1cad5f4017d6247973ed2d02e4368bd8e4
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-08 17:50:21 +0300

    created views for badges

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,59 +1,63 @@
-class Admin::BadgesController < Admin::BaseController
+# frozen_string_literal: true
 
-  before_action :set_bades, only: %i[index]
-  before_action :find_badge, only: %i[show edit update destroy]
+module Admin
+  class BadgesController < Admin::BaseController
+    
+    before_action :set_bades, only: %i[index]
+    before_action :find_badge, only: %i[show edit update destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_badge_not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_badge_not_found
 
-  def index; end
+    def index; end
 
-  def show; end
+    def show; end
 
-  def new
-    @badge = Badge.new
-  end
-
-  def edit; end
-
-  def create
-    @badge = current_user.badges.new(badge_params)
-
-    if @badge.save
-      redirect_to admin_badge_path(@badge), notice: t('.success')
-    else
-      render :new
+    def new
+      @badge = Badge.new
     end
-  end
 
-  def update
-    if @badge.update(badge_params)
-      redirect_to admin_badge_path(@badge), notice: t('.success')
-    else
-      render :edit
+    def edit; end
+
+    def create
+      @badge = current_user.badges.new(badge_params)
+
+      if @badge.save
+        redirect_to admin_badge_path(@badge), notice: t('.success')
+      else
+        render :new
+      end
     end
-  end
 
-  def destroy
-    @badge.destroy
+    def update
+      if @badge.update(badge_params)
+        redirect_to admin_badge_path(@badge), notice: t('.success')
+      else
+        render :edit
+      end
+    end
 
-    redirect_to admin_badges_path, notice: t('.success')
-  end
+    def destroy
+      @badge.destroy
 
-  private
+      redirect_to admin_badges_path, notice: t('.success')
+    end
 
-  def set_badges
-    @badges = Badge.all
-  end
+    private
 
-  def find_badge
-    @badge = Badge.find(params[:id])
-  end
+    def set_badges
+      @badges = Badge.all
+    end
 
-  def badge_params
-    params.require(:badge).permit(:title, :image, :rule_mame, :rule_value)
-  end
+    def find_badge
+      @badge = Badge.find(params[:id])
+    end
 
-  def rescue_with_badge_not_found
-    render plain: t('.badge')
+    def badge_params
+      params.require(:badge).permit(:title, :image, :rule_mame, :rule_value)
+    end
+
+    def rescue_with_badge_not_found
+      render plain: t('.badge')
+    end
   end
 end
