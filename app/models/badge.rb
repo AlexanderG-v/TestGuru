77f6ee6fb7e1dd313a1d468f63fe03410b751c2c
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-08 14:30:37 +0300

    added validation to badge model

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,4 +3,7 @@
 class Badge < ApplicationRecord
   has_many :badges_users, dependent: :destroy
   has_many :users, through: :badges_users
+
+  validates :title, :image, :rule_name, :rule_value, presence: true
+  validates :title, uniqueness: true
 end
