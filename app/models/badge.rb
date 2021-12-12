d124dd2749b8e152456fe06fcc4b1fd5fc9c36a9
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-12 18:50:44 +0300

    using enum

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Badge < ApplicationRecord
-  BADGES_RULES = %w[all_tests_by_category pass_test_by_level first_test_passed].freeze
+  # BADGES_RULES = %w[all_tests_by_category pass_test_by_level first_test_passed].freeze
+
+  enum badges_rules: {
+    all_tests_by_category: 0,
+    pass_test_by_level: 1,
+    first_test_passed: 2
+  }
 
   has_many :badges_users, dependent: :destroy
   has_many :users, through: :badges_users
