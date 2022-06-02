dd8476a7a7a8d58145594888598e2cab8c235947
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-06-02 23:58:46 +0300

    Add ENV for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ categories = Category.create!([
 
 users = User.create!([
                        { first_name: 'Александр', last_name: 'Петров', type: 'Admin', 
-                         email: 'alexandr@gmail.com', password: '123456' },
+                         email: ENV['SEEDS_USERNAME'], password: ENV['SEEDS_PASSWORD'] },
                        { first_name: 'Петр', last_name: 'Петров', type: 'User', 
                          email: 'petr@gmail.com', password: '123456' }
                      ])
